459457d624735d7623c41f0c51bf698c0ae75be7
Matus <matus1142@gmail.com>
2025-07-24 17:47:51 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ padding: "2rem", textAlign: "center" }}>
+      <h1>MathQuiz Frontend is running 🎉</h1>
+      <p>This is your React starting point.</p>
     </div>
   );
 }
